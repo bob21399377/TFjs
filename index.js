8fab0f0be3ffc77fd09a146de63994c9a26f8056
bob21399377 <bob21399377@gmail.com>
2021-08-23 14:47:18 +0800

    index
    1.0

---
+++ index.js
@@ -0,0 +1,21 @@
+console.log(tf.version.tfjs)
+Model();
+async function Model() {
+    const model = await tf.loadLayersModel('https://raw.githubusercontent.com/bob21399377/TFjs/main/model/model.json');
+
+    let fake_input1 = [0.1];
+    let fake_input2 = [0.1];
+    for(let i = 1;i<1*30*6*2;i++){
+        fake_input1.push(Math.random());
+        fake_input2.push(Math.random());
+    }
+    for (let i = 0;i<1*30*6;i++){
+        fake_input1.push(Math.random());
+    }
+    let a = tf.tensor4d(fake_input1,[1,30,6,3],dtype='float32');
+    let b = tf.tensor4d(fake_input2,[1,30,6,2],dtype='float32');
+
+    const ouput = model.predict([a, b]);
+    console.log(ouput.arraySync())
+    document.getElementById("result").innerHTML = ouput.arraySync()[0][0];
+}
